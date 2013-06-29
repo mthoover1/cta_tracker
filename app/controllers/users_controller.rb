083bf151370a18d70b9d4f606f8c826fc025b333
Matthew Hoover <Matt@MattBookAir.local>
2013-06-29 15:55:55 -0500

    included helpers in app_controller, updated route for profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,13 @@ class UsersController < ApplicationController
     user = User.new(params[:user])
     if user.save
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to '/profile'
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 end
