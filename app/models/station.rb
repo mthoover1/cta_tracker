4eb14ac9945a548cb72720713ad68da9077d31b7
Matthew Hoover <Matt@MattBookAir.local>
2013-06-29 11:13:43 -0500

    fix typo in model associations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,8 @@
 class Station < ActiveRecord::Base
   has_many :stops, :dependent => :destroy
-  has_many :connections, :depenedent => :destroy
+  has_many :connections, :dependent => :destroy
   has_many :lines, :through => :connections
-  has_many :user_stations, :depenedent => :destroy
+  has_many :user_stations, :dependent => :destroy
   has_many :users, :through => :user_stations
 
   validates_presence_of :name, :cta_id
