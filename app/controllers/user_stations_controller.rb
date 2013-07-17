36eb709047ec614bcdb5602aff3d94d279b1ad49
Matthew Hoover <mthoover1@gmail.com>
2013-07-17 09:03:12 -0500

    Add Station button works with javascript

--- app/controllers/user_stations_controller.rb
+++ app/controllers/user_stations_controller.rb
@@ -1,5 +1,9 @@
 class UserStationsController < ApplicationController
   def create
+  	p current_user.id
+  	@station = Station.find_by_id(params[:station_id])
+  	current_user.stations << @station
+  	redirect_to station_path(@station)
   end
 
   def destroy
